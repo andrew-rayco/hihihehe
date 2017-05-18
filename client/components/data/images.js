4d14962905279b754176f31873442e4c93c48031
Sarah Dueweke <sdueweke@gmail.com>
2017-05-19 11:47:36 +1200

    redo image urls

---
+++ client/components/data/images.js
@@ -0,0 +1,23 @@
+const images = [
+    'https://avatars0.githubusercontent.com/u/80690?v=3&s=460',
+    'https://avatars2.githubusercontent.com/u/24601704?v=3&s=460',
+    'https://avatars1.githubusercontent.com/u/25892646?v=3&s=460',
+    'https://avatars2.githubusercontent.com/u/25452399?v=3&s=460',
+    'https://avatars2.githubusercontent.com/u/25465309?v=3&s=460',
+    'https://avatars1.githubusercontent.com/u/25610428?v=3&s=460',
+    'https://avatars0.githubusercontent.com/u/3125856?v=3&s=400',
+    'https://avatars2.githubusercontent.com/u/23735378?v=3&s=400',
+    'https://avatars1.githubusercontent.com/u/25041420?v=3&s=460',
+    'https://avatars3.githubusercontent.com/u/25041420?v=3&s=400',
+    'https://avatars3.githubusercontent.com/u/72309?v=3&s=460',
+    'https://avatars0.githubusercontent.com/u/25888248?v=3&s=400',
+    'https://avatars3.githubusercontent.com/u/25002466?v=3&s=400',
+    'https://avatars1.githubusercontent.com/u/25476337?v=3&s=400',
+    'https://avatars2.githubusercontent.com/u/20761533?v=3&s=460',
+    'https://avatars0.githubusercontent.com/u/25313000?v=3&s=460',
+    'https://avatars2.githubusercontent.com/u/15813732?v=3&s=460',
+    'https://avatars0.githubusercontent.com/u/24973297?v=3&s=460',
+    'https://avatars1.githubusercontent.com/u/25238976?v=3&s=460',
+    'https://avatars3.githubusercontent.com/u/25414477?v=3&s=460'
+
+]
