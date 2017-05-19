d3aaf44070520f687bd75a93dcb9039c9201d765
Andrew <andy@andycondon.com>
2017-05-19 15:46:06 +1200

    working

--- client/components/Facestorm.jsx
+++ client/components/Facestorm.jsx
@@ -16,14 +16,57 @@ class Facestorm extends React.Component {
   // }
 
 
+  constructor (props) {
+    super(props)
+
+    this.windowWidth = window.innerWidth
+    this.windowHeight = window.innerHeight
+
+    const children = []
+
+    this.state = {
+      children: [{
+        width: this.windowWidth / 2,
+        height: this.windowWidth / 2,
+        image: randomPic(images)
+      }]
+    }
+
+
+  }
+  newChild(images) {
+    console.log("new child");
+    let newChildren = [...this.state.children]
+    newChildren.push({
+      width: Math.floor(Math.random() * this.windowWidth ),
+      height: Math.floor(Math.random() * this.windowHeight ),
+      image: randomPic(images)
+    })
+    this.setState({children: newChildren})
+  }
+
+  componentDidMount() {
+    console.log("mounted");
+    setInterval(() => {
+      this.newChild(images)
+    }, 921)
+  }
+
+  renderChild(child) {
+    return <img src={child.image}/>
+  }
+
+  renderChildren(children) {
+    return children.map(this.renderChild)
+  }
+
+
   render() {
     return (
 
       <div className="Facestorm">
-
-
+        {this.renderChildren(this.state.children)}
         <h1> Facestorm </h1>
-        <img src={randomPic(images)}/>
           <audio id='bev' src="/sounds/bev.m4a" autoPlay={true}>
               Your browser does not support the <code>audio</code> element.
           </audio>
@@ -36,4 +79,6 @@ class Facestorm extends React.Component {
 function randomPic(datafile) {
   return datafile[Math.floor(Math.random() * 20) + 1].image
 }
+
+
 export default Facestorm
